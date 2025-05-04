2ef524fb6cc37ad951b2b05433befe03575b7a00
demixjr <168976116+demixjr@users.noreply.github.com>
2025-05-05 02:17:50 +0300

    feat: UI

---
+++ UI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using BLL;
+
+namespace Lab_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceFacade facade = new ServiceFacade();
+            ConsoleMenu consoleMenu = new ConsoleMenu(facade);
+            consoleMenu.Menu();
+        }
+    }
+}
